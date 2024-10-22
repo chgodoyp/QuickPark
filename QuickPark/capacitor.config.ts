e7565f990f75b9e91a74deb92058aff8284d27a6
Christian Godoy <81322806+chgodoyp@users.noreply.github.com>
2024-10-22 00:23:59 -0300

    Delete QuickPark directory

--- QuickPark/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'QuickPark',
-  webDir: 'www'
-};
-
-export default config;
