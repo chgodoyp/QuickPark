7ad6b7ecb76c90cd02af12d6630b75e3bc079cb1
chgodoyp <chgodoyp@gmail.com>
2024-11-17 13:16:53 -0300

    ACT A Version 3

--- Quickpark.1/QuickPark/src/app/register/register.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RegisterPage } from './register.page';
-import { RegisterPageRoutingModule } from './register-routing.module';
-
-@NgModule({
-  imports: [CommonModule, RegisterPageRoutingModule]})
